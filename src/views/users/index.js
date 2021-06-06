84ebbd9e373bd8bad8c7dc56ec17b1f2bd497e4b
Tariq Khan <45430120+Thk101@users.noreply.github.com>
2021-06-06 13:17:47 -0400

    updated

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -10,9 +10,58 @@ import axios from "axios";
 
 //fake api : JSONPLACEHOLDER
 function Users() {
-  const fetchUser = () => {};
+  const [users, setUsers] = React.useState();
+  const [userName, setUserName] = React.useState("");
+  ///get request
+  const fetchUser = async () => {
+    return await axios.get("https://jsonplaceholder.typicode.com/users");
+  };
 
-  return <div>User </div>;
+  const postUser = async (para) => {
+    return await axios.post("https://jsonplaceholder.typicode.com/users", para);
+  };
+  //initial render
+  React.useEffect(() => {
+    fetchUser()
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const userNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+  //CRUD
+  const seeSubmit = () => {
+    alert(userName);
+
+    postUser({ username: userName })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  ///post
+
+  return (
+    <div>
+      User
+      {users?.map((user, index) => (
+        <h1>
+          name: {user.name} username: {user.username}
+        </h1>
+      ))}
+      <input
+        type="text"
+        placeholder="Enter Username"
+        onChange={userNameChange}
+      />
+      <button onClick={seeSubmit}>Submit</button>
+    </div>
+  );
 }
 
 export default Users;
